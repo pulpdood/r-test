ae87aefa16cc11f576fab8d39fedb5c1b055945d
Kevin Yip <kevinyipeio@gmail.com>
2021-02-16 17:23:37 +1100

    first commit

---
+++ R/hello.R
@@ -0,0 +1,28 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  library(tidyverse)
+  library(ggpubr)
+  theme_set(
+    theme_bw() +
+      theme(legend.position = "top")
+  )
+  p <- ggplot(mtcars, aes(mpg, wt)) +
+    geom_point() +
+    geom_smooth(method = lm) +
+    stat_cor(method = "pearson", label.x = 20)
+  p
+}
